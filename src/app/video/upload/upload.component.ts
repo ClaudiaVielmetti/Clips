d0a4f459792a614486e0ef7839961f371f7ea570
ClaudiaV27 <claudiavielmetti38@gmail.com>
2023-08-08 16:16:33 +0200

    The upload form

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-upload',
@@ -9,6 +10,17 @@ export class UploadComponent implements OnInit {
   isDragover = false;
   file: File | null = null;
   nextStep = false;
+
+  title = new FormControl('', {
+    validators : [
+      Validators.required,
+      Validators.minLength(3)
+    ],
+    nonNullable: true });
+
+  uploadForm = new FormGroup({
+    title: this.title
+  })
 
   constructor() { }
 
@@ -24,7 +36,15 @@ export class UploadComponent implements OnInit {
       return;
     }
 
+    this.title.setValue(
+      this.file.name.replace(/\.[^/.]+$/, '')
+    )
+
     this.nextStep = true;
+  }
+
+  uploadFile() {
+    console.log('File uploaded');
   }
 
 }
