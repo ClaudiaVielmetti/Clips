d9b6e046c13603e90d7dbfcaafce49e640a6115a
ClaudiaV27 <claudiavielmetti38@gmail.com>
2023-08-09 11:16:30 +0200

    Upload alert component

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -12,6 +12,10 @@ export class UploadComponent implements OnInit {
   isDragover = false;
   file: File | null = null;
   nextStep = false;
+  showAlert = false;
+  alertColor = 'blue';
+  alertMsg = 'Please wait! We are uploading your video.';
+  inSubmission = false;
 
   title = new FormControl('', {
     validators : [
@@ -46,6 +50,11 @@ export class UploadComponent implements OnInit {
   }
 
   uploadFile() {
+    this.showAlert = true;
+    this.alertColor = 'blue';
+    this.alertMsg = 'Please wait! We are uploading your video.';
+    this.inSubmission = true;
+
     const clipFileName = uuid();
     const clipPath = `clips/${clipFileName}.mp4`;
 
