13c0fe9b5048b6237faaa772ee9019239a20639d
ClaudiaV27 <claudiavielmetti38@gmail.com>
2023-08-09 12:02:42 +0200

    Storing file data

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -2,7 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
 import { v4 as uuid } from 'uuid';
-import { last } from 'rxjs/operators';
+import { last, switchMap } from 'rxjs/operators';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import firebase from 'firebase/compat/app';
+
 
 @Component({
   selector: 'app-upload',
@@ -19,6 +22,9 @@ export class UploadComponent implements OnInit {
   inSubmission = false;
   percentage = 0;
   showPercentage = false;
+  user: firebase.User | null = null
+
+
   title = new FormControl('', {
     validators : [
       Validators.required,
@@ -30,7 +36,13 @@ export class UploadComponent implements OnInit {
     title: this.title
   })
 
-  constructor(private storage: AngularFireStorage) { }
+  constructor(
+    private storage: AngularFireStorage,
+    private auth: AngularFireAuth
+
+    ){
+      auth.user.subscribe(user => this.user = user)
+    }
 
   ngOnInit(): void {
   }
@@ -62,14 +74,28 @@ export class UploadComponent implements OnInit {
     const clipPath = `clips/${clipFileName}.mp4`;
 
     const task = this.storage.upload(clipPath, this.file);
+    const clipRef = this.storage.ref(clipPath);
 
     task.percentageChanges().subscribe(progress => {
       this.percentage = progress as number / 100;
     });
 
-    task.snapshotChanges().pipe(last()
+    task.snapshotChanges().pipe
+    (last(),
+    switchMap(() => clipRef.getDownloadURL())
     ).subscribe({
-      next: (snapshot) => {
+      next: (url) => {
+        const clip = {
+          uid: this.user?.uid,
+          displayName: this.user?.displayName,
+          title: this.title.value,
+          fileName: `${clipFileName}.mp4`,
+          url
+
+        }
+
+        console.log(clip)
+
         this.alertColor = 'green';
         this.alertMsg = 'Success! Your video is uploaded.';
         this.showPercentage = false;
