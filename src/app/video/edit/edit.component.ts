53d7b632c2067c99b09dc7ffbeb9598dea2716b2
ClaudiaV27 <claudiavielmetti38@gmail.com>
2023-08-10 10:41:29 +0200

    update list of clips

--- src/app/video/edit/edit.component.ts
+++ src/app/video/edit/edit.component.ts
@@ -1,14 +1,21 @@
-import { Component, OnInit, OnDestroy, Input, OnChanges, SimpleChanges } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  OnDestroy,
+  Input,
+  OnChanges,
+  Output,
+  EventEmitter,
+} from '@angular/core';
 import { ModalService } from 'src/app/services/modal.service';
 import IClip from 'src/app/models/clip.model';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ClipService } from 'src/app/services/clip.service';
 
-
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
-  styleUrls: ['./edit.component.css']
+  styleUrls: ['./edit.component.css'],
 })
 export class EditComponent implements OnInit, OnDestroy, OnChanges {
   @Input() activeClip: IClip | null = null;
@@ -16,26 +23,21 @@ export class EditComponent implements OnInit, OnDestroy, OnChanges {
   showAlert = false;
   alertColor = 'blue';
   alertMsg = 'Please wait! Updating clip.';
+  @Output() update = new EventEmitter();
 
   clipID = new FormControl('', {
-    nonNullable: true
-})
+    nonNullable: true,
+  });
   title = new FormControl('', {
-    validators: [
-      Validators.required,
-      Validators.minLength(3)
-    ],
-    nonNullable: true
-  })
+    validators: [Validators.required, Validators.minLength(3)],
+    nonNullable: true,
+  });
   editForm = new FormGroup({
     title: this.title,
-    id: this.clipID
-  })
+    id: this.clipID,
+  });
 
-  constructor(
-    private modal: ModalService,
-    private clipService: ClipService
-    ) { }
+  constructor(private modal: ModalService, private clipService: ClipService) {}
 
   ngOnInit(): void {
     this.modal.register('editClip');
@@ -46,36 +48,39 @@ export class EditComponent implements OnInit, OnDestroy, OnChanges {
   }
 
   ngOnChanges(): void {
-    if(!this.activeClip) {
-      return
+    if (!this.activeClip) {
+      return;
     }
+    this.inSubmission = false;
+    this.showAlert = false;
     this.clipID.setValue(this.activeClip.docID as string);
     this.title.setValue(this.activeClip.title);
   }
 
   async submit() {
+    if(!this.activeClip) {
+      return;
+    }
+
     this.inSubmission = true;
     this.showAlert = true;
     this.alertColor = 'blue';
     this.alertMsg = 'Please wait! Updating clip.';
 
-    try{
-      await this.clipService.updateClip(
-        this.clipID.value,
-        this.title.value
-      )
-    }
-
-    catch(e) {
+    try {
+      await this.clipService.updateClip(this.clipID.value, this.title.value);
+    } catch (e) {
       this.inSubmission = false;
       this.alertColor = 'red';
       this.alertMsg = 'Something went wrong! Please try again later.';
-      return
+      return;
     }
+
+    this.activeClip.title = this.title.value;
+    this.update.emit(this.activeClip);
 
     this.inSubmission = false;
     this.alertColor = 'green';
     this.alertMsg = 'Success!';
   }
-
 }
